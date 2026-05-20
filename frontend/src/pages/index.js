2dd79e5d55629b58ccac3961ffff271426e39505
DarwinMelaya <melayadarwin@gmail.com>
2026-05-20 10:12:19 +0800

    Add landing page and Auht page

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+// Landing
+export { default as LandingPage } from "./Landing/LandingPage";
+
+// Auth
+export { default as Login } from "./Auth/Login";
+export { default as SignUp } from "./Auth/SignUp";
